da819a2e4192f99603e5597ede021c573eebbaab
cavalostotosos <tomazini.antonio@escola.pr.gov.br>
2025-10-26 12:05:02 -0300

    Add files via upload

---
+++ wildlifes.js
@@ -0,0 +1,16 @@
+
+
+const myobeserver = new IntersectionObserver( (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach( (element) => myobeserver.observe(element))
+
